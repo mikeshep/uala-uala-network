4f5793c59a16d0659633628f6bf852dd7503af40
Miguel Olmedo <miguel.olmedo@globant.com>
2021-03-02 16:51:18 -0600

    [Feat] Set Meal vars public

--- Sources/UalaNetwork/UalaDomain/Meal.swift
+++ Sources/UalaNetwork/UalaDomain/Meal.swift
@@ -9,23 +9,23 @@ import Foundation
 
 public struct Meal: Codable {
     public let idMeal, strMeal: String
-    let strDrinkAlternate: JSONNull?
+    public let strDrinkAlternate: JSONNull?
     public let strCategory, strArea, strInstructions: String
-    let strMealThumb: String
-    let strTags: String?
+    public let strMealThumb: String
+    public let strTags: String?
     public let strYoutube: String
-    let strIngredient1, strIngredient2, strIngredient3, strIngredient4: String
-    let strIngredient5, strIngredient6: String
-    let strIngredient7, strIngredient8, strIngredient9, strIngredient10: String
-    let strIngredient11, strIngredient12, strIngredient13, strIngredient14: String
-    let strIngredient15, strIngredient16, strIngredient17, strIngredient18: String
-    let strIngredient19, strIngredient20, strMeasure1, strMeasure2: String
-    let strMeasure3, strMeasure4, strMeasure5, strMeasure6: String
-    let strMeasure7, strMeasure8, strMeasure9, strMeasure10: String
-    let strMeasure11, strMeasure12, strMeasure13, strMeasure14: String
-    let strMeasure15, strMeasure16, strMeasure17, strMeasure18: String
-    let strMeasure19, strMeasure20, strSource: String
-    let dateModified: JSONNull?
+    public let strIngredient1, strIngredient2, strIngredient3, strIngredient4: String
+    public let strIngredient5, strIngredient6: String
+    public let strIngredient7, strIngredient8, strIngredient9, strIngredient10: String
+    public let strIngredient11, strIngredient12, strIngredient13, strIngredient14: String
+    public let strIngredient15, strIngredient16, strIngredient17, strIngredient18: String
+    public let strIngredient19, strIngredient20, strMeasure1, strMeasure2: String
+    public let strMeasure3, strMeasure4, strMeasure5, strMeasure6: String
+    public let strMeasure7, strMeasure8, strMeasure9, strMeasure10: String
+    public let strMeasure11, strMeasure12, strMeasure13, strMeasure14: String
+    public let strMeasure15, strMeasure16, strMeasure17, strMeasure18: String
+    public let strMeasure19, strMeasure20, strSource: String
+    public let dateModified: JSONNull?
 }
 
 
